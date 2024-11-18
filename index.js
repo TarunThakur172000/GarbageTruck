99f50bbc59f0ad286643510a47915d744d7ce7b2
Tarun Thakur <75246602+TarunThakur172000@users.noreply.github.com>
2024-11-18 11:48:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const connectDB = require('./db');
 const candidateRoutes = require('./routes/candidateRoutes');
 const authRoutes = require('./routes/authRoutes');
-
+var cors = require('cors')
 require('dotenv').config();
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 // Connect to MongoDB
 connectDB();
 
